431c1e3677cae68ac494461ace60616148bc55af
jangc <202130323@email.daelim.ac.kr>
2026-05-19 00:42:58 +0900

    11주차

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
-import 'package:unitask/app/extensions/snackbar_extension.dart';
+import 'package:unitask/core/extensions/build_context_extension.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
